2faab5c92d2619b4711a744ee690923b38accf1c
daniel <danielbernaldezescalada@gmail.com>
2019-08-18 12:02:57 +0200

    primerHttpComunicacionControler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  // The NG module for using @angular/common/http
+  HttpClientModule
+} from  '@angular/common/http';
 
 
 /*Components*/
@@ -7,6 +11,7 @@ import { AppRoutingModule } from './components/app/app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { ComunicacionComponent } from './components/comunicacion/comunicacion.component';
 
 /*Service   vacio   */
 
@@ -14,11 +19,13 @@ import { UserComponent } from './components/user/user.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent
-  ],
+    UserComponent,
+    ComunicacionComponent
+      ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // para hacer peticiones a STS
   ],
   providers: [], /* van aquí servicios y cosas queramos importar*/
   bootstrap: [AppComponent]  /*componente principal aplicación */
---
+++ src/app/components/comunicacion/comunicacion.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-comunicacion',
+  templateUrl: './comunicacion.component.html',
+  styleUrls: ['./comunicacion.component.scss']
+})
+export class ComunicacionComponent implements OnInit {
+  data : Object;
+  loading : boolean;
+  http: HttpClient;
+
+
+
+  constructor( http: HttpClient) { 
+ this.http = http;
+  }
+  ngOnInit() {
+  }
+
+makeRequest():void{
+  // funcion  para pedir información. 
+  this.loading=true;
+  this.http
+    .get ('http://localhost:8080/demo/hello')
+    .subscribe(data=>{
+         this.data=data;
+         this.loading=false;
+    })
+  
+
+}
+
+}
